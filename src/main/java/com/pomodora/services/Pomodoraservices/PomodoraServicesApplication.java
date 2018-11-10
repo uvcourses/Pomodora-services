667bbdc6acda52ca03b848ff5f342be1c83c87b5
Yuvaraj <uvcourses@gmail.com>
2018-11-10 14:19:38 -0600

    first commit

---
+++ src/main/java/com/pomodora/services/Pomodoraservices/PomodoraServicesApplication.java
@@ -0,0 +1,12 @@
+package com.pomodora.services.Pomodoraservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoraServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoraServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pomodora/services/Pomodoraservices/PomodoraServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pomodora.services.Pomodoraservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PomodoraServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
