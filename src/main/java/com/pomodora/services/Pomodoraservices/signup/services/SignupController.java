f5162a22ffd529a76de2ff3a1be915c54c4bdbc7
Yuvaraj <uvcourses@gmail.com>
2018-11-13 22:56:02 -0600

    service Tested

--- src/main/java/com/pomodora/services/Pomodoraservices/signup/services/SignupController.java
+++ src/main/java/com/pomodora/services/Pomodoraservices/signup/services/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +43,7 @@ public class SignupController {
 		//LOG.info("Getting user with ID: {}.", userId);
 		return signupRepository.findById(userId);
 	}
+	@CrossOrigin(origins="http://localhost:8080")
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public SignupFormBean addNewUsers(@RequestBody SignupFormBean user) {
 		//LOG.info("Saving user.");
